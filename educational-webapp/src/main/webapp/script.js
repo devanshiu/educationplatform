e91c1eefe3626bc56084be3fbe9ef769e7fb6f54
devanshi <devanshi.udeshi@gmail.com>
2020-07-23 09:44:08 +0000

    MVP version of colors memory game

--- educational-webapp/src/main/webapp/script.js
+++ educational-webapp/src/main/webapp/script.js
@@ -79,3 +79,120 @@ function alphabetAudio(letter) {
         zAudio.play();
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  //card options
+  const cardArray = [
+    {
+      name: 'blue',
+      img: 'images/colors/blue.jpg'
+    },
+    {
+      name: 'green',
+      img: 'images/colors/green.jpg'
+    },
+    {
+      name: 'red',
+      img: 'images/colors/red.jpg'
+    },
+    {
+      name: 'yellow',
+      img: 'images/colors/yellow.jpg'
+    },
+    {
+      name: 'red',
+      img: 'images/colors/red.jpg'
+    },
+    {
+      name: 'black',
+      img: 'images/colors/black.jpg'
+    },
+    {
+      name: 'blue',
+      img: 'images/colors/blue.jpg'
+    },
+    {
+      name: 'brown',
+      img: 'images/colors/brown.jpg'
+    },
+    {
+      name: 'yellow',
+      img: 'images/colors/yellow.jpg'
+    },
+    {
+      name: 'brown',
+      img: 'images/colors/brown.jpg'
+    },
+    {
+      name: 'green',
+      img: 'images/colors/green.jpg'
+    },
+    {
+      name: 'black',
+      img: 'images/colors/black.jpg'
+    }
+  ]
+
+  cardArray.sort(() => 0.5 - Math.random())
+
+  const grid = document.querySelector('.grid')
+  const resultDisplay = document.querySelector('#result')
+  var cardsChosen = []
+  var cardsChosenId = []
+  const cardsWon = []
+
+  //create your board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement('img')
+      card.setAttribute('src', 'images/colors/card.png')
+      card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
+      grid.appendChild(card)
+    }
+  }
+
+  //check for matches
+  function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    
+    if(optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute('src', 'images/colors/card.png')
+      cards[optionTwoId].setAttribute('src', 'images/colors/card.png')
+      alert('You have clicked the same image!')
+    }
+    else if (cardsChosen[0] === cardsChosen[1]) {
+      alert('You found a match')
+      cards[optionOneId].setAttribute('src', 'images/colors/white.png')
+      cards[optionTwoId].setAttribute('src', 'images/colors/white.png')
+      cards[optionOneId].removeEventListener('click', flipCard)
+      cards[optionTwoId].removeEventListener('click', flipCard)
+      cardsWon.push(cardsChosen)
+    } else {
+      cards[optionOneId].setAttribute('src', 'images/colors/card.png')
+      cards[optionTwoId].setAttribute('src', 'images/colors/card.png')
+      alert('Sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if  (cardsWon.length === cardArray.length/2) {
+      resultDisplay.textContent = 'Congratulations! You found them all!'
+    }
+  }
+
+  //flip your card
+  function flipCard() {
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length ===2) {
+      setTimeout(checkForMatch, 500)
+    }
+  }
+
+  createBoard()
+})
